9343fbab5827ae513b723e7b95da355664eb92af
Matías Di Iorio <45548452+MatiDiyo@users.noreply.github.com>
2020-10-08 16:25:35 -0300

    Framework created

---
+++ MoviePass/Config/Autoload.php
@@ -0,0 +1,15 @@
+<?php 
+    namespace Config;
+        
+        class Autoload 
+        {
+            public static function Start() {
+                spl_autoload_register(function($className)
+                {
+                    $classPath = ucwords(str_replace("\\", "/", ROOT.$className).".php");
+                    
+                    include_once($classPath);
+                });
+            }
+        }
+?>
---
+++ MoviePass/Views/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH ?>bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH ?>estilos.css">
+
+    <title>MoviePass</title>
+</head>
+<body>
