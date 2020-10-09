4e3054e1353e87db714e20c2b8d375f48e785645
Franco <francoperaltagg@gmail.com>
2020-10-09 14:16:44 -0300

    primer commit

---
+++ MoviePass/Views/nav-bar.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">MoviePass</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    
+      <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" id="cartelera" href="<?php echo FRONT_ROOT."Movie/refreshData"?>">Cartelera</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="login" href="#">Iniciar sesión</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="login" href="#">Registrarse</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="login" href="#">Agregar cine</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="login" href="#">listar cines</a>
+          </li>
+      </ul>
+    </div>
+    
+  </div>
+</nav>
