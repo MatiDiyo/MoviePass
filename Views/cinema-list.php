1845df9cc3fd951628555396d25aa5cd0b974260
Hugo Puyol <chaplisimo@gmail.com>
2020-10-12 20:01:22 -0300

    mergeo de cambios visuales

--- Views/cinema-list.php
+++ Views/cinema-list.php
@@ -5,15 +5,26 @@
  
 ?>
 
-<!--
+
 <div id="listaCines">
     <div class="container">
-        <div class="listcinema-container"><h2>Administraci&oacute;n de Cines</h2>
+        <div class="listcinema-container">
+            <div class="row">
+                <div class="col-9">
+                    <h2>Sucursales</h2>
+                </div>
+                <div class="col-3">
+                    <form method="get" action="<?php echo FRONT_ROOT."Cinema/ShowAddView"?>">
+                        <button type="submit" class="btn btn-dark" >Agregar nuevo cine</button>
+                    </form>                        
+                </div>
+            </div>  
+            <hr/>
             <div class="row">
                 <?php
                     foreach($cinemaList as $cine){
                 ?>
-                    <div class="col-3">
+                    <div class="col-3" style="margin-bottom:20px;">
                         <div class="card">
                             <div class="card-img-top cinema-card container">
                                     <div class="row" style="height:inherit">
@@ -26,74 +37,25 @@
                                 <!--<span class="h5 card-title"><?php echo $cine->getName() ?></span><br/>-->
                                 <span class="h5 card-title">Direccion: <?php echo $cine->getAddress() ?></span><br/>
                                 <span class="p card-text">Capacidad: <?php echo $cine->getCapacity() ?></span><br/>
-                                <span class="p card-text">Precio: <?php echo $cine->getPrice() ?></span>
+                                <span class="p card-text">Precio: $<?php echo $cine->getPrice() ?></span>
+                                <!--<div class="col-auto">-->
+                                    <form class="" action="<?php echo FRONT_ROOT."Cinema/Remove"?>">
+                                        <button type="submit" name="id" class="btn-dark btn-sm" value="<?php echo $cine->getId() ?>">Eliminar</button>
+                                        <!--probablemente haya que poner un boton mas para administrar el cine-->
+                                    </form>
+                                <!--</div>-->
                             </div>    
                         </div>
                     </div>
-                <?php
+            <?php
                     }
                 ?>
             </div>
             <hr/>
-            <div>
-                <a href="<?php echo FRONT_ROOT."Cinema/ShowAddView"?>"><i class="fa fa-plus-square" style="font-size:80px"></i></a>
-            </div>
         </div>
 
     </div>
 </div>
--->
-<div id="listaCines">
-    <div class="container">
-        <div class="listcinema-container">
-            <div class="row">
-                <div class="col-9">
-                    <h2>Sucursales.</h2>
-                </div>
-                <div class="col-3">
-                    <form method="get" action="<?php echo FRONT_ROOT."Cinema/ShowAddView"?>">
-                        <button type="submit" class="btn btn-dark" >Agregar nuevo cine</button>
-                    </form>                        
-                </div>
-            </div>  
-            <hr>
-
-            <form action="<?php echo FRONT_ROOT."Cinema/Remove"?>">
-   
-                    <?php
-                        foreach($cinemaList as $cine){
-                    ?>
-
-                        <div class="row">
-                            <div class="col-9">
-                                <span>Cine: <?php echo $cine->getName() ?>.</span>
-                                <br/>
-                                <span>Direccion: <?php echo $cine->getAddress() ?>.</span>
-                                <br/>
-                                <span>Capacidad: <?php echo $cine->getCapacity() ?> personas.</span>
-                                <br/>
-                                <span>Precio: $<?php echo $cine->getPrice() ?>.</span>
-                            </div>
-                            <div class="col-3">
-                                <button type="submit" name="id" class="btn btn-dark" value="<?php echo $cine->getId() ?>">Eliminar</button>
-                                <!--probablemente haya que poner un boton mas para administrar el cine-->
-                            </div>
-                        </div>
-                        <hr>
-                    <?php
-                        }
-                    ?>
-
-            </form>
-
-        </div>
-    </div>
-</div>
-
-
-
-
-
 
 <?php
  
